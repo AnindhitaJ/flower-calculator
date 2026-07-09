8db4fed894df78b9eb9f6f9d45fa316f4eea5680
AnindhitaJ <100269959+AnindhitaJ@users.noreply.github.com>
2026-07-09 13:35:39 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 let n=0;
-function addFlower(){n++;let x=document.createElement('div');x.className='flower';x.innerHTML=`Bunga ${n}<input class=q value=1><select class=h><option value=5000>Besar 5000</option><option value=3000>Sedang 3000</option><option value=1000>Kecil 1000</option></select>`;flowers.appendChild(x)}
+function addFlower(){
+n++;
+let d=document.createElement('div');
+d.className='flower';
+d.innerHTML=`
+<div class="flower-head">🌸 Bunga ${n}</div>
+<div class="two">
+<input value="1" placeholder="Jumlah">
+<select>
+<option>Besar - Rp5.000</option>
+<option>Sedang - Rp3.000</option>
+<option>Kecil - Rp1.000</option>
+</select>
+</div>`;
+flowers.appendChild(d);
+}
 addFlower();
-function num(id){return Number(document.getElementById(id).value||0)}
-function hitung(){let t=0;document.querySelectorAll('.flower').forEach(x=>t+=num2(x.querySelector('.q').value)*num2(x.querySelector('.h').value));t+=num('b8')*5000+num('b12')*18000+num('b20')*30000;t+=num('cqty')*num('cprice')+num('pqty')*num('pprice')+num('dqty')*num('dprice')+num('plqty')*num('plprice')+num('pbqty')*num('pbprice');t+=num('listrik')+num('tenaga')+num('jasa');let j=t+(t*num('profit')/100);hasil.innerHTML='Modal Rp'+t.toLocaleString('id-ID')+'<br>Harga Jual Rp'+Math.round(j).toLocaleString('id-ID')}
-function num2(x){return Number(x||0)}
