73d382a9e399d170542b6fee6d0b295a5a5ec688
AnindhitaJ <100269959+AnindhitaJ@users.noreply.github.com>
2026-07-09 12:39:15 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-let counter=0;
+let count=0;
+const rupiah=n=>"Rp"+Number(n).toLocaleString("id-ID");
 
 function addItem(){
-counter++;
-let div=document.createElement("div");
-div.className="item";
-div.innerHTML=`
-<div class="title">🌸 Item ${counter}</div>
-<select class="harga">
-<option value="5000">Bunga Besar - 5000</option>
-<option value="3000">Bunga Sedang - 3000</option>
-<option value="1000">Bunga Kecil - 1000</option>
-<option value="3000">Cello - 3000</option>
-<option value="1000">Tissu - 1000</option>
+count++;
+let d=document.createElement("div");
+d.className="item";
+d.innerHTML=`
+<b>🌸 Bunga ${count}</b>
+<select class="price">
+<option value="5000">Besar - Rp5.000</option>
+<option value="3000">Sedang - Rp3.000</option>
+<option value="1000">Kecil - Rp1.000</option>
 </select>
-<input class="qty" value="1" type="number">
-`;
-document.getElementById("items").appendChild(div);
+<input class="qty" value="1" type="number">`;
+document.getElementById("items").appendChild(d);
 }
-
 addItem();
+
+function formatInput(el){
+el.addEventListener("blur",()=> {
+el.value=Number(el.value.replace(/\D/g,"")).toLocaleString("id-ID");
+});
+}
+["listrik","tenaga","jasa"].forEach(id=>formatInput(document.getElementById(id)));
+
+function num(id){
+return Number(document.getElementById(id).value.replace(/\./g,"")||0);
+}
 
 function hitung(){
 let modal=0;
-
 document.querySelectorAll(".item").forEach(x=>{
-modal+=Number(x.querySelector(".harga").value)*Number(x.querySelector(".qty").value);
+modal+=Number(x.querySelector(".price").value)*Number(x.querySelector(".qty").value);
 });
-
-modal+=Number(boneka.value)*5000;
-modal+=Number(boneka2.value)*18000;
-modal+=Number(boneka3.value)*30000;
-
-modal+=Number(listrik.value)+Number(tenaga.value)+Number(jasa.value);
+modal+=num("b1")*5000+num("b2")*18000+num("b3")*30000;
+modal+=num("listrik")+num("tenaga")+num("jasa");
 
 let jual=modal+(modal*Number(profit.value)/100);
-
-document.getElementById("modal").innerHTML="Rp"+modal.toLocaleString("id-ID");
-document.getElementById("jual").innerHTML="Rp"+Math.round(jual).toLocaleString("id-ID");
+document.getElementById("modal").innerHTML=rupiah(modal);
+document.getElementById("jual").innerHTML=rupiah(Math.round(jual));
 }
