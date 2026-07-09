68b62d03e0e789c08a48ec092d9e02b3860c5fbc
AnindhitaJ <100269959+AnindhitaJ@users.noreply.github.com>
2026-07-09 12:41:42 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
-let count=0;
-const rupiah=n=>"Rp"+Number(n).toLocaleString("id-ID");
-
-function addItem(){
-count++;
-let d=document.createElement("div");
-d.className="item";
-d.innerHTML=`
-<b>🌸 Bunga ${count}</b>
-<select class="price">
-<option value="5000">Besar - Rp5.000</option>
-<option value="3000">Sedang - Rp3.000</option>
-<option value="1000">Kecil - Rp1.000</option>
-</select>
-<input class="qty" value="1" type="number">`;
-document.getElementById("items").appendChild(d);
+function angka(id){
+return Number(document.getElementById(id).value.replace(/[^0-9]/g,'')||0);
 }
-addItem();
 
-function formatInput(el){
-el.addEventListener("blur",()=> {
-el.value=Number(el.value.replace(/\D/g,"")).toLocaleString("id-ID");
+function formatInput(id){
+let el=document.getElementById(id);
+el.value='Rp'+Number(el.value.replace(/[^0-9]/g,'')).toLocaleString('id-ID');
+
+el.addEventListener('blur',()=>{
+el.value='Rp'+Number(el.value.replace(/[^0-9]/g,'')).toLocaleString('id-ID');
 });
 }
-["listrik","tenaga","jasa"].forEach(id=>formatInput(document.getElementById(id)));
 
-function num(id){
-return Number(document.getElementById(id).value.replace(/\./g,"")||0);
-}
+['listrik','tenaga','jasa'].forEach(formatInput);
 
 function hitung(){
-let modal=0;
-document.querySelectorAll(".item").forEach(x=>{
-modal+=Number(x.querySelector(".price").value)*Number(x.querySelector(".qty").value);
-});
-modal+=num("b1")*5000+num("b2")*18000+num("b3")*30000;
-modal+=num("listrik")+num("tenaga")+num("jasa");
-
+let modal=angka('listrik')+angka('tenaga')+angka('jasa');
 let jual=modal+(modal*Number(profit.value)/100);
-document.getElementById("modal").innerHTML=rupiah(modal);
-document.getElementById("jual").innerHTML=rupiah(Math.round(jual));
+
+result.innerHTML=
+`Total Modal<br><b>Rp${modal.toLocaleString('id-ID')}</b><br><br>
+Harga Jual<br><b>Rp${Math.round(jual).toLocaleString('id-ID')}</b>`;
 }
